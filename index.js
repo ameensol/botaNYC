22cc5bb5bfb5b94958bb714788b1ae3249302dde
Ameen Soleimani <ameensol@gmail.com>
2015-02-02 00:17:46 -0500

    converted index.js to single jobs -- using heroku scheduler addon

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
 var Scraper = require('./scrape')
 var async = require('async')
 var fs = require('fs')
-var server = require('./server') // starts the webserver
 
-// every hour, check for new job postings
-var scrape = function (wait) {
-  setTimeout(function() {
-    var scraper = new Scraper()
+var scraper = new Scraper()
 
-    scraper.start(function(err, jobs) {
-      if (err) throw err
-      console.log(jobs)
-      // for each job listing, check if a job listing with that id exists in
-      // the database. If it does, do nothing. If it doesn't, save it to the
-      // database and send out a tweet.
+scraper.start(function(err, jobs) {
+  if (err) return console.log(err)
+  console.log(jobs)
+  // for each job listing, check if a job listing with that id exists in
+  // the database. If it does, do nothing. If it doesn't, save it to the
+  // database and send out a tweet.
 
-      var oldJobs = JSON.parse(fs.readFileSync('jobs.json'))
+  var oldJobs = JSON.parse(fs.readFileSync('jobs.json'))
 
-      var newJobs = jobs.filter(function (job) {
-        var matching = oldJobs.filter(function (oldJob) {
-          return oldJob.id == job.id
-        })
-        return matching.length === 0
-      })
-
-      newJobs = newJobs.map(function (job) {
-        job.created = new Date().getTime()
-        return job
-      })
-
-      console.log('There are ' + newJobs.length + ' new jobs')
-      var allJobs = oldJobs.concat(newJobs)
-      fs.writeFileSync('jobs.json', JSON.stringify(allJobs))
-      console.log('Saved Jobs')
-
-      scrape(6*60*60*1000)
-
+  var newJobs = jobs.filter(function (job) {
+    var matching = oldJobs.filter(function (oldJob) {
+      return oldJob.id == job.id
     })
-  }, wait)
-}
+    return matching.length === 0
+  })
 
-scrape(0) // don't wait before the first scrape
+  console.log('There are ' + newJobs.length + ' new jobs')
+  if (!newJobs || newJobs.length === 0) return
+
+  newJobs = newJobs.map(function (job) {
+    job.created = new Date().getTime()
+    return job
+  })
+
+  var allJobs = oldJobs.concat(newJobs)
+  fs.writeFileSync('jobs.json', JSON.stringify(allJobs))
+  console.log('Saved new Jobs')
+})
